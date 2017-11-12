8c5008aad40ee0afcd254bbb2fde81193fe3adb7
Midhun Dandamudi <midhunsezhi@gmail.com>
2017-11-12 13:02:40 -0800

    Configure and initialize the weather app

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Yahoo! Weather App</h1>
+        </header>
+        <p>This is the initial set up for the weather app</p>
+      </div>
+    );
+  }
+}
+
+export default App;
